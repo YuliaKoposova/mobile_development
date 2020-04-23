e28d6d9b23d04d5574289207c2b9c0e631f8c2b1
YuliaKoposova <33418174+YuliaKoposova@users.noreply.github.com>
2020-04-23 14:23:58 +0300

    Add files via upload

---
+++ Lab 2/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Lab_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(2, 3);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Berry;
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Трафик ";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1527, 436);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            title1.Name = "Title1";
+            title1.Text = "График зависимости трафика от времени (в байтах)";
+            this.chart1.Titles.Add(title1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(186, 454);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(462, 454);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1576, 498);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Lab 2/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Globalization;
+
+namespace Lab_2 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            string[] uniqueTime = new string[100];
+            double[] traffics = new double[100];
+            string sumTrafficString = "";
+            string MString = "";
+            _Main.mainFunction(ref uniqueTime, ref traffics, ref sumTrafficString, ref MString);
+            label1.Text = $"Всего {sumTrafficString} Кб";
+            label2.Text = $"{MString} рублей";
+            for (int i = 0; i < 80; i++) {
+                chart1.Series[0].Points.AddXY(uniqueTime[i], traffics[i]);
+            }
+        }
+    }
+
+    class _Main {
+        public static void mainFunction(ref string[] uniqueTime, ref double[] traffics, ref string sumTrafficString, ref string MString) {
+            double sumTraffic = 0;
+            Parsing Parser = new Parsing();
+            Tariffication Tarificator = new Tariffication();
+            Parser.CreateArray();
+            Parser.GetValues(ref sumTraffic, ref uniqueTime, ref traffics);
+            Tarificator.CalculationService(ref sumTraffic, ref sumTrafficString, ref MString);
+        }
+    }
+
+    class Parsing {
+        string[] NFR = File.ReadAllLines("NetFlow.csv");
+        string abonent = "192.168.250.3";
+        string[] row;
+        string[,] abonentRecords = new string[4774, 50];
+        int recordsCounter = 0;
+
+        public void CreateArray() {
+            for (int i = 0; i < NFR.Length; i++) {
+                if (NFR[i].Contains(abonent)) {
+                    row = NFR[i].Split(',');
+                    for (int j = 0; j < row.Length; j++) {
+                        abonentRecords[recordsCounter, j] = row[j];
+                    }
+                    recordsCounter++;
+                }
+            }
+        }
+
+        public void GetValues(ref double sumTraffic, ref string[] uniqueTime, ref double[] traffics) {
+            CultureInfo _invert = CultureInfo.InvariantCulture;
+            sumTraffic = 0;
+            int count = 0;
+            int uniqueTimeCount = 0;
+            bool check = false;
+            DateTime[] fullTime = new DateTime[4774];
+            string[] shortTime = new string[4774];
+            for (int i = 0; i < abonentRecords.GetLength(0); i++) {
+                if (abonentRecords[i, 3] == abonent || abonentRecords[i, 4] == abonent) {
+                    sumTraffic += Convert.ToDouble(abonentRecords[i, 12], _invert);
+                    fullTime[count] = Convert.ToDateTime(abonentRecords[i, 0]);
+                    shortTime[count] = fullTime[count].ToShortTimeString();
+                    count++;
+                }
+            }
+
+            for (int i = 0; i < shortTime.Length; i++) {
+                for (int j = 0; j < uniqueTimeCount + 1; j++) {
+                    if (shortTime[i] == uniqueTime[j]) {
+                        traffics[j] += Convert.ToDouble(abonentRecords[i, 12], _invert);
+                        check = true;
+                        break;
+                    }
+                }
+                if (check == false) {
+                    uniqueTime[uniqueTimeCount] = shortTime[i];
+                    traffics[uniqueTimeCount] += Convert.ToDouble(abonentRecords[i, 12], _invert);
+                    uniqueTimeCount++;
+                }
+                check = false;
+            }
+        }
+    }
+
+    class Tariffication {
+        double kTraffic = 0.5;
+        double M;
+
+        public void CalculationService(ref double sumTraffic, ref string sumTrafficString, ref string MString) {
+            sumTraffic = sumTraffic / 1024; //в килобайтах
+            M = (sumTraffic - 1000) * kTraffic;
+            sumTraffic = Math.Round(sumTraffic);
+            M = Math.Round(M);
+            sumTrafficString = Convert.ToString(sumTraffic);
+            MString = Convert.ToString(M);
+        }
+    }
+}
